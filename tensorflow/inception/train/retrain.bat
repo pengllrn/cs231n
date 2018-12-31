fd7a4a4cd4beff3335a7d1fd04b59ab1e4c53852
pengllrn <1256880407@qq.com>
2018-12-31 19:20:38 +0800

    retrain a flower classifier based on inception-v3

---
+++ tensorflow/inception/train/retrain.bat
@@ -0,0 +1,8 @@
+python3 E:/Pycharm/Project/NNetword/Tensorflow/inception/retrain.py ^
+--bottleneck_dir bottleneck ^
+--how_many_training_steps 200 ^
+--model_dir E:/Pycharm/Project/NNetword/Tensorflow/inception/inception_model ^
+--output_graph output_graph.pb ^
+--output_labels output_labels.txt ^
+--image_dir data/train/
+pause
